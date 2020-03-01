e2deaa3aa3972643af07764a54f2fcb15fe9ecfc
unknown <hegburgers-are-fat-free@live.ca>
2020-02-29 20:15:52 -0700

    Basic

--- app/move_snake.py
+++ app/move_snake.py
@@ -6,7 +6,7 @@ def get_move(data):
     #directions = ["up", "down", "left", "right"]
     directions = survival_choices(data) #get bad options, remove them from contention
     
-    if (len(directions <= 0)):
+    if (len(directions) <= 0):
         directions = ["up", "down", "left", "right"]
 
     direction = random.choice(directions)
