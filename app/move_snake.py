a238608c0c881b624f56c0222a4f05dfcf406344
unknown <hegburgers-are-fat-free@live.ca>
2020-02-29 23:35:16 -0700

    Find Food

--- app/move_snake.py
+++ app/move_snake.py
@@ -13,6 +13,9 @@ def get_move(data):
     food_directions = consumption_choices(data, directions)
 
     if (food_directions != None):
+        for direction in food_directions:
+            if (direction not in directions):
+                food_directions.remove(direction)
         directions = food_directions
 
     #no path availabe that won't kill us, so just return any response
