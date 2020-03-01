cd2c0e92807ca8223a1d4b96453597268569beba
unknown <hegburgers-are-fat-free@live.ca>
2020-02-29 19:29:44 -0700

    Basic

--- app/move_snake.py
+++ app/move_snake.py
@@ -4,7 +4,7 @@ from app.survive import survival_choices
 
 def get_move(data):
     #directions = ["up", "down", "left", "right"]
-    directions = survival_choices() #get bad options, remove them from contention
+    directions = survival_choices(data) #get bad options, remove them from contention
     
     if (len(directions <= 0)):
         directions = ["up", "down", "left", "right"]
