72f1e3b36872939dfd453afe3b84489d7acb653a
unknown <hegburgers-are-fat-free@live.ca>
2020-04-05 19:08:52 -0600

    Longest path

--- app/survive.py
+++ app/survive.py
@@ -213,7 +213,7 @@ def flood_fill(data, walls, available_directions, aStar):
         else:
             longest_path, closest_tail = find_longest_path(data, get_location_from_direction(flood_directions[i][0],x,y), flood_areas[i])
             #traverse longest path, and see if path to tail opens up during path traversal, if so, area is viable to go through
-            if (traverse_longest_path(aStar, data, longest_path, closest_tail)):
+            if (traverse_longest_path(data, longest_path, closest_tail)):
                 final_directions.append(flood_directions[i][0])
 
         #TODO
