04216f4dda9263a9e1954bb111ae50ba08417d41
unknown <hegburgers-are-fat-free@live.ca>
2020-04-05 19:12:16 -0600

    Longest path

--- app/survive.py
+++ app/survive.py
@@ -212,6 +212,7 @@ def flood_fill(data, walls, available_directions, aStar):
         #If snake goes to area smaller than itself, start moving through and occuping space and see if space opens up, or path to tail open
         else:
             longest_path, closest_tail = find_longest_path(data, get_location_from_direction(flood_directions[i][0],x,y), flood_areas[i])
+            print("longest_path: " + str(longest_path))
             #traverse longest path, and see if path to tail opens up during path traversal, if so, area is viable to go through
             if (traverse_longest_path(data, longest_path, closest_tail)):
                 final_directions.append(flood_directions[i][0])
